16b6e123da937e54c6509d470f5612c9aeaa035f
Davis Walker <dave@happybits.co>
2021-03-26 08:39:36 -0400

    Initial Commit

---
+++ Shared/audio_red_flashApp.swift
@@ -0,0 +1,17 @@
+//
+//  audio_red_flashApp.swift
+//  Shared
+//
+//  Created by Dave Walker on 3/26/21.
+//
+
+import SwiftUI
+
+@main
+struct audio_red_flashApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
